48e23722cdda6538c6a5d1de91bd0c89aa9a9773
danieleliwag-eng <danieleliwag@gmail.com>
2026-01-30 19:04:56 +0800

    Add files via upload

---
+++ coding project/back end developer/reportcard (1).py
@@ -0,0 +1,86 @@
+chineselanguage = str(input("enter the chinese grade:"))
+englishlangauge = int(input("enter the english grade:"))
+mathematics = str(input("enter the mathematics grade:"))
+biology = str(input("enter the biology grade:"))
+informationtechnology = str(input("enter the information technology grade:"))
+appliedlearning = str(input("enter thr applied learning:"))
+
+chineselangauge = 53
+enlgishlanguage = 44
+mathematics = 98
+biology = 56
+informationtechnology = 78
+appliedlearning = 40
+
+
+if chineselangauge > 100:
+    print("A")
+elif chineselangauge > 80:
+    print("B")
+elif chineselangauge > 60:
+    print("C")
+elif chineselangauge > 50:
+    print("D")
+else:
+    chineselangauge < 40
+    print("F")
+
+if englishlangauge > 99 :
+    print("A")
+elif englishlangauge > 80:
+    print("b")
+elif  englishlangauge > 60:
+    print("C")
+elif englishlangauge > 50:
+    print("D")
+else:
+    englishlangauge < 40
+    print("F")
+
+if mathematics > 100:
+    print("A")
+elif mathematics > 80:
+    print("B")
+elif mathematics > 60:
+    print("C")
+elif mathematics > 50:
+    print("D")
+else:
+    mathematics < 40
+    print("F")
+
+if biology > 100:
+    print("A")
+elif biology > 80:
+    print("B")
+elif biology > 60:
+    print("C")
+elif biology > 50:
+    print("D")
+else:
+    biology < 40
+    print("F")
+
+if informationtechnology > 100:
+    print("A")
+elif informationtechnology > 80:
+    print("B")
+elif informationtechnology > 60:
+    print("C")
+elif informationtechnology > 50:
+    print("D")
+else:
+    informationtechnology < 40
+    print("F")
+
+if appliedlearning > 100:
+    print("A")
+elif appliedlearning < 80:
+    print("B")
+elif appliedlearning > 60:
+    print("C")
+elif appliedlearning >  50:
+    print("D")
+else:
+    appliedlearning < 40
+    print("F")
